54d56a39361a19dd96757ace2346e73921dd44cc
Thiago <th.framos@gmail.com>
2021-05-22 17:28:11 -0300

    useCallBack

---
+++ src/views/examples/UseCallbackButton.jsx
@@ -0,0 +1,14 @@
+import React, {memo}from 'react';
+
+
+const UseCallbackButton = props =>{
+  console.log("render...")
+  return(
+    <div>
+      <button className="btn" onClick={()=>props.inc(6)}> +6</button>
+      <button className="btn" onClick={()=>props.inc(12)}> +12</button>
+      <button className="btn" onClick={()=>props.inc(18)}> +18</button>
+    </div>
+  )
+}
+export default memo(UseCallbackButton)
