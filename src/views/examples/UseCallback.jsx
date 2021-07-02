756ee909d18bf172b344bd8e6cdda7894d2c9043
Thiago <th.framos@gmail.com>
2021-07-02 15:45:09 -0300

    UseCallBack

--- src/views/examples/UseCallback.jsx
+++ src/views/examples/UseCallback.jsx
@@ -1,27 +1,38 @@
-import React, {useState, useCallback} from 'react'
-import PageTitle from '../../components/layout/PageTitle'
-import UseCallbackButton from './UseCallbackButton'
+import React, { useState, useCallback } from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+import UseCallbackButton from './UseCallbackButton';
 
 const UseCallback = (props) => {
-    const [count, setcount] = useState(0);
-    
+  const [count, setcount] = useState(0);
+  const [text, setText] = useState('tes');
 
-    const inc = useCallback((delta)=>{
-        setcount(curr=> curr + delta)
-    },[setcount])
-   
-    return (
-        <div className="UseCallback">
-            <PageTitle
-                title="Hook UseCallback"
-                subtitle="Retorna uma função memoizada!"
-            />
-            <div className="center">
-                <span className="text">{count}</span>
-                <UseCallbackButton inc={inc}/>
-            </div>
-        </div>
-    )
-}
+  console.log('render...1');
+  const inc = useCallback(
+    (delta) => {
+      setcount((curr) => curr + delta);
+    },
+    [setcount]
+  );
+  const read = useCallback(
+    (delta) => {
+      console.log(delta);
+      setText((current) => delta);
+    },
+    [setText]
+  );
 
-export default UseCallback
+  return (
+    <div className="UseCallback">
+      <PageTitle
+        title="Hook UseCallback"
+        subtitle="Retorna uma função memoizada!"
+      />
+      <div className="center">
+        <span className="text">{count}</span>
+        <UseCallbackButton inc={inc} read={read} valueRead={text} />
+      </div>
+    </div>
+  );
+};
+
+export default UseCallback;
--- src/views/examples/UseCallbackButton.jsx
+++ src/views/examples/UseCallbackButton.jsx
@@ -1,14 +1,26 @@
-import React, {memo}from 'react';
+import React, { memo } from 'react';
 
-
-const UseCallbackButton = props =>{
-  console.log("render...")
-  return(
+const UseCallbackButton = (props) => {
+  console.log('render...2');
+  return (
     <div>
-      <button className="btn" onClick={()=>props.inc(6)}> +6</button>
-      <button className="btn" onClick={()=>props.inc(12)}> +12</button>
-      <button className="btn" onClick={()=>props.inc(18)}> +18</button>
+      <button className="btn" onClick={() => props.inc(6)}>
+        {' '}
+        +6
+      </button>
+      <button className="btn" onClick={() => props.inc(12)}>
+        {' '}
+        +12
+      </button>
+      <button className="btn" onClick={() => props.inc(18)}>
+        {' '}
+        +18
+      </button>
+      <input
+        value={props.valueRead}
+        onChange={(x) => props.read(x.target.value)}
+      />
     </div>
-  )
-}
-export default memo(UseCallbackButton)
+  );
+};
+export default memo(UseCallbackButton);
